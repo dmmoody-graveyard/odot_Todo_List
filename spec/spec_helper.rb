548824e0181864297eca9fdbaa73ccd2f086025b
Duane M. Moody <dmmoody@gmail.com>
2014-06-11 21:53:16 -0400

    Add validations for description

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
+RSpec.configure do |c|
+  c.expose_current_running_example_as :example
+end
+
 RSpec.configure do |config|
   # ## Mock Framework
   #
